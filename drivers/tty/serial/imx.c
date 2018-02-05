0a72b5984ce435d21f80b55aa65d4181bcda68ac
Lukas Wunner <lukas@wunner.de>
2018-02-05 18:16:15 +0100

    serial: imx: Support common rs485 binding for RTS polarity
    Invoke the ->rs485_config callback on probe to adjust the initial RTS
    polarity based on the UART's device properties.
    
    This implicitly fixes a bug:  If RTS control is not available, rs485
    should be disabled even if it was enabled through a device property.
    Log an error when that occurs.
    
    Cc: Sascha Hauer <s.hauer@pengutronix.de>
    Signed-off-by: Lukas Wunner <lukas@wunner.de>
    Acked-by: Uwe Kleine-König <u.kleine-koenig@pengutronix.de>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/tty/serial/imx.c
+++ drivers/tty/serial/imx.c
@@ -2095,6 +2095,11 @@ static int serial_imx_probe(struct platform_device *pdev)
 	}
 	sport->port.uartclk = clk_get_rate(sport->clk_per);
 
+	if (sport->port.rs485.flags & SER_RS485_ENABLED && !sport->have_rtscts)
+		dev_err(&pdev->dev, "no RTS control, disabling rs485\n");
+
+	imx_rs485_config(&sport->port, &sport->port.rs485);
+
 	if (!is_imx1_uart(sport) && sport->dte_mode) {
 		/*
 		 * The DCEDTE bit changes the direction of DSR, DCD, DTR and RI
