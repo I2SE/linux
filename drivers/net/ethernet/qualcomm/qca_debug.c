8d64b4d5c760ab71ef7949f866b4c7b2f72a676f
Stefan Wahren <stefan.wahren@i2se.com>
2018-07-06 14:57:23 +0200

    net: qca_spi: Introduce register verification
    This introduce a simple read/write register verification. This is
    disabled per default and can be enabled via module parameter
    qcaspi_verify.
    
    Signed-off-by: Michael Heimpold <michael.heimpold@i2se.com>
    Signed-off-by: Stefan Wahren <stefan.wahren@i2se.com>

--- drivers/net/ethernet/qualcomm/qca_debug.c
+++ drivers/net/ethernet/qualcomm/qca_debug.c
@@ -60,6 +60,8 @@ static const char qcaspi_gstrings_stats[][ETH_GSTRING_LEN] = {
 	"Write buffer misses",
 	"Transmit ring full",
 	"SPI errors",
+	"SPI rx verify errors",
+	"SPI tx verify errors",
 };
 
 #ifdef CONFIG_DEBUG_FS
