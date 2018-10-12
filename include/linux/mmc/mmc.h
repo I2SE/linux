f92fa97648e3b6808e2c7bb4f011a6c52ae8f359
Stefan Wahren <stefan.wahren@i2se.com>
2018-10-12 14:01:10 +0200

    mmc: Print info for write reliability settings
    Signed-off-by: Stefan Wahren <stefan.wahren@i2se.com>

--- include/linux/mmc/mmc.h
+++ include/linux/mmc/mmc.h
@@ -290,6 +290,7 @@ struct _mmc_csd {
 #define EXT_CSD_BKOPS_START		164	/* W */
 #define EXT_CSD_SANITIZE_START		165     /* W */
 #define EXT_CSD_WR_REL_PARAM		166	/* RO */
+#define EXT_CSD_WR_REL_SET		167	/* R/W */
 #define EXT_CSD_RPMB_MULT		168	/* RO */
 #define EXT_CSD_FW_CONFIG		169	/* R/W */
 #define EXT_CSD_BOOT_WP			173	/* R/W */
