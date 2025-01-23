b8736ec8d46ccff1699c15c60ab6f2620f6d8e34
Stefan Wahren <stefan.wahren@chargebyte.com>
2025-01-23 10:18:35 +0100

    net: vertexcom: mse102x: Fix possible stuck of SPI interrupt
    The MSE102x doesn't provide any SPI commands for interrupt handling.
    So in case the interrupt fired before the driver requests the IRQ,
    the interrupt will never fire again. In order to fix this always poll
    for pending packets after opening the interface.
    
    Fixes: 2f207cbf0dd4 ("net: vertexcom: Add MSE102x SPI support")
    Signed-off-by: Stefan Wahren <stefan.wahren@chargebyte.com>

--- drivers/net/ethernet/vertexcom/mse102x.c
+++ drivers/net/ethernet/vertexcom/mse102x.c
@@ -507,6 +507,7 @@ static irqreturn_t mse102x_irq(int irq, void *_mse)
 static int mse102x_net_open(struct net_device *ndev)
 {
 	struct mse102x_net *mse = netdev_priv(ndev);
+	struct mse102x_net_spi *mses = to_mse102x_spi(mse);
 	unsigned long flags = IRQF_ONESHOT;
 	int ret;
 
@@ -525,6 +526,13 @@ static int mse102x_net_open(struct net_device *ndev)
 	netif_start_queue(ndev);
 
 	netif_carrier_on(ndev);
+
+	/* The SPI interrupt can stuck in case of pending packet(s).
+	 * So poll for possible packet(s) to re-arm the interrupt.
+	 */
+	mutex_lock(&mses->lock);
+	mse102x_rx_pkt_spi(mse);
+	mutex_unlock(&mses->lock);
 
 	netif_dbg(mse, ifup, ndev, "network device up\n");
 
